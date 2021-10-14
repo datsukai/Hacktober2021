8fea9ce91bf719fe46b4695b284c57031587d883
datsukai <melissa.meline@gmail.com>
2021-10-13 23:18:22 -0700

    Hacktober2021

---
+++ FirstProgram/src/MyFirstApp/MyFirstApp.java
@@ -0,0 +1,9 @@
+package MyFirstApp;
+
+public class MyFirstApp {
+//Using strings to print out text
+		public static void main (String[] args) {
+			System.out.println("I Rule!");
+			System.out.println("The World");
+		}
+}
---
+++ FirstProgram/src/program/HelloWorld.java
@@ -0,0 +1,8 @@
+package program;
+
+public class HelloWorld {
+//First project in Java!
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
