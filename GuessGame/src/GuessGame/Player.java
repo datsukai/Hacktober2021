ab08069b4d08d16c9ce388c4aeb6d81076aff6db
datsukai <melissa.meline@gmail.com>
2021-10-13 23:42:55 -0700

    GuessGame Java Project
    Hacktober2021

---
+++ GuessGame/src/GuessGame/Player.java
@@ -0,0 +1,12 @@
+package GuessGame;
+
+public class Player {
+int number = 0;
+
+public void guess() {
+	number = (int) (Math.random() * 10);
+	System.out.println("I'm guessing " + number);
+}
+}
+
+
